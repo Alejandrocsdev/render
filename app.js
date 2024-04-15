f37b7afa0befb3f516d11e8e4080b63aee00362c
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-04-15 13:41:59 +0800

    test back

---
+++ app.js
@@ -0,0 +1,17 @@
+const leopardo = require('leopardo')
+const app = leopardo()
+const port = 3000
+
+app.use(leopardo.cors())
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Leopardo package index page!')
+})
+
+app.get('/test', (req, res) => {
+  res.send('This is the test page.')
+})
+
+app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`)
+})
